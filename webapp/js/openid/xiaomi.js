d76b79ca78082b1a72c9e0d13b35942f34cb9a50
caols <caols@y-car.com.cn>
2015-07-07 16:32:40 +0800

    接入小米openid 。js

--- webapp/js/openid/xiaomi.js
+++ webapp/js/openid/xiaomi.js
@@ -1,7 +1,17 @@
 /**
  * Created by caols on 15-6-5.
  */
-var snsapi = 'https://account.xiaomi.com/oauth2/authorize?client_id=2882303761517266846&response_type=token&redirect_uri=http%3A%2F%2F' + host + 'xiaomi%2Fopen_id.html';
+var situation = "test";
+var appId = loadCfg('data.json', function(enviroment) {
+    if ('dev' === enviroment['thisis'] || 'staging' === enviroment['thisis']) {
+        situation = "test";
+        return '2882303761517356288';
+    } else {
+        situation = "production";
+        return '2882303761517266846';
+    }
+});
+var snsapi = 'https://account.xiaomi.com/oauth2/authorize?client_id='+ appId +'&response_type=token&redirect_uri=http%3A%2F%2F' + host + 'xiaomi%2Fopen_id.html';
 
 var reqParam = getReqParam(), to_snsapi = true;
 var accessToken = reqParam['access_token'];//小米返回的access_token值
@@ -9,42 +19,16 @@ var macKey = reqParam['mac_key'];//获取小米userId需要用到的macKey
 if (accessToken && macKey) {
     alert(accessToken);
     alert(macKey);
-    //var url = '/carusers/account/userid/range/'+accessToken+'/'+macKey;
-    //var obj;
-    //obj = $.ajax({
-    //    type: type,
-    //    url: url,
-    //    data: data,
-    //    dataType: "json",
-    //    contentType: "application/json",
-    //    cache: false,
-    //    async: false,
-    //    timeout: 15 * 1000,
-    //    success: function (data) {
-    //        if (data != null) {
-    //            var phoneNumber = data[2];
-    //            var openId = data[1];
-    //            getStore().set('user_mobile', phoneNumber);
-    //            getStore().set("open_id", openId);
-    //
-    //            postReq("login_by_xiaomi_openid.json?code="+openId,{},function(data){
-    //                updateUser(data);
-    //                after_login();
-    //            },function(data){
-    //                if(data && data['code'] == '10007'){
-    //                    show_login_win();
-    //                }
-    //            });
-    //        }
-    //    },
-    //    beforeSend: function () {
-    //        //showLoading();
-    //    },
-    //    complete: function () {
-    //        //hideLoading();
-    //    }
-    //});
-
+    postReq("login_by_xiaomi_openid.json?code="+accessToken+"&situation="+situation,{},function(data){
+        getStore().set("open_id",data.openid);
+        updateUser(data);
+        after_login();
+    },function(data){
+        getStore().set("open_id",data.data);
+        if(data && data['code'] == '10007'){
+            show_login_win();
+        }
+    });
     to_snsapi = false;
 }
 
