0d5a01032d39458da10dc36431758876822c9a56
caols <caols@y-car.com.cn>
2015-07-13 18:54:46 +0800

    优惠券背景图片需要单独拷贝

--- deploy_tools/libs/_make_h5.sh
+++ deploy_tools/libs/_make_h5.sh
@@ -26,12 +26,19 @@ function after_xiaomi {
     echo 'done'
 }
 
+function copy_coupons_imgs {
+    echo 'copy coupons imgs' &&
+    cp -a $1/webapp/img/couponsbackground*.png $2/img/ &&
+    echo 'done'
+}
+
 deploy=${10}
 
 if [ ${deploy:0:1} == 'y' ]; then
     mkdir -p $1/$3/$4 &&
     fis release -com -r $1/webapp/ -f $1/"fis-cfgs"/"fis-"$2"-"$4"-conf.js" -d $1/$3/$4 -D &&
     cp -a $1/webapp/js/openid/* $1/$3/$4/js/openid &&
+    copy_coupons_imgs $1 $1/$3/$4 &&
     bash _cp_json.sh $1/enviroment/$2 $1/platform/$4 $1/$3/$4
 fi
 
@@ -39,6 +46,7 @@ if [ ${deploy:1:1} == 'y' ]; then
     mkdir -p $1/$3/$5 &&
     fis release -com -r $1/webapp/ -f $1/"fis-cfgs"/"fis-"$2"-"$5"-conf.js" -d $1/$3/$5 -D &&
     cp -a $1/webapp/js/openid/* $1/$3/$5/js/openid &&
+    copy_coupons_imgs $1 $1/$3/$5 &&
     bash _cp_json.sh $1/enviroment/$2 $1/platform/$5 $1/$3/$5
 fi
 
@@ -48,6 +56,7 @@ if [ ${deploy:2:1} == 'y' ]; then
     fis release -com -r $1/tmp/ -f $1/"fis-cfgs"/"fis-"$2"-"$6"-conf.js" -d $1/$3/$6 -D &&
     #after_xiaomi $1 &&
     cp -a $1/webapp/js/openid/* $1/$3/$6/js/openid &&
+    copy_coupons_imgs $1 $1/$3/$6 &&
     bash _cp_json.sh $1/enviroment/$2 $1/platform/$6 $1/$3/$6
 fi
 
@@ -55,6 +64,7 @@ if [ ${deploy:3:1} == 'y' ]; then
     mkdir -p $1/$3/$7 &&
     fis release -com -r $1/webapp/ -f $1/"fis-cfgs"/"fis-"$2"-"$7"-conf.js" -d $1/$3/$7 -D &&
     cp -a $1/webapp/js/openid/* $1/$3/$7/js/openid &&
+    copy_coupons_imgs $1 $1/$3/$7 &&
     bash _cp_json.sh $1/enviroment/$2 $1/platform/$7 $1/$3/$7
 fi
 
@@ -62,6 +72,7 @@ if [ ${deploy:4:1} == 'y' ]; then
     mkdir -p $1/$3/$8 &&
     fis release -com -r $1/webapp/ -f $1/"fis-cfgs"/"fis-"$2"-"$8"-conf.js" -d $1/$3/$8 -D &&
     cp -a $1/webapp/js/openid/* $1/$3/$8/js/openid &&
+    copy_coupons_imgs $1 $1/$3/$8 &&
     bash _cp_json.sh $1/enviroment/$2 $1/platform/$8 $1/$3/$8
 fi
 
@@ -71,6 +82,7 @@ if [ ${deploy:5:1} == 'y' ]; then
     fis release -com -r $1/tmp/ -f $1/"fis-cfgs"/"fis-"$2"-"$9"-conf.js" -d $1/$3/$9 -D &&
     #after_xiaomi $1 &&
     cp -a $1/webapp/js/openid/* $1/$3/$9/js/openid &&
+    copy_coupons_imgs $1 $1/$3/$9 &&
     bash _cp_json.sh $1/enviroment/$2 $1/platform/$9 $1/$3/$9
 fi
 
