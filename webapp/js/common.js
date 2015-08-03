e7bfc23c4dd18ed400bdb15a1c0b3c066a3f1959
caols <caols@y-car.com.cn>
2015-08-03 13:34:43 +0800

    js回调

--- webapp/js/common.js
+++ webapp/js/common.js
@@ -19,8 +19,7 @@ var domain = '', api_root = "/v1/api/", dev = '';
     }
     loadCfg('platform.json', function (platform) {
         if ('rc' === platform['platform']) {
-            domain = 'http://dev.yangaiche.com';
-            dev = '/develop';
+            domain = '/java/dev';
         }
     });
 }();
