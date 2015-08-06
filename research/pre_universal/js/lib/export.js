7eea00475c848eb0c9a2808088392830b98676d4
caols <caols@y-car.com.cn>
2015-08-06 17:29:29 +0800

    区分浏览器+模块化

--- research/pre_universal/js/lib/export.js
+++ research/pre_universal/js/lib/export.js
@@ -4,16 +4,20 @@ var yangaiche;
 // default modules
 var local_storage = 'local_storage';
 var load = 'load';
+var site_config = 'site_config';
 var browser_type = 'browser_type';
 
 !function ($) {
 
-    function exist(callback) {
-        return callback !== undefined && callback !== null;
+    function exist(obj) {
+        return obj !== undefined && obj !== null;
     }
 
     var runtime_obj = {};
     yangaiche = function (name, callback, params) {
+        if (exist(runtime_obj[name])) {
+            return runtime_obj[name];
+        }
         if (exist(callback)) {
             runtime_obj[name] = callback(params);
         }
@@ -65,6 +69,7 @@ var browser_type = 'browser_type';
     yangaiche(load, function () {
         var loaded = [];
         return function (url) {
+            console.log(loaded);
             if (!loaded.includes(url)) {
                 var head = document.getElementsByTagName('head')[0], script = document.createElement('script');
                 script.type = 'text/javascript';
@@ -75,18 +80,46 @@ var browser_type = 'browser_type';
         };
     });
 
+    yangaiche(site_config, function() {
+        var site_config = null;
+        return function() {
+            if (!exist(site_config)) {
+                site_config = $.ajax({
+                    url: './map/site.json',
+                    cache: false,
+                    async: false,
+                    dataType: 'json'
+                })['responseJSON'];
+            }
+            console.log(site_config);
+            return site_config;
+        }();
+    });
+
     yangaiche(browser_type, function () {
         var type = null;
         return function () {
             if (!type) {
-                var ua = window.navigator.userAgent.toLowerCase();
-                return ua;
+                var user_agent = window.navigator.userAgent.toLowerCase();
+                console.log(user_agent);
+                for(var i = 0; i < yangaiche(site_config)['browser'].length; i++) {
+                    var b = yangaiche(site_config)['browser'][i];
+                    if (user_agent.indexOf(b['userAgent']) >= 0) {
+                        type = b['type'];
+                        if ('normal' !== b['type']) {
+                            return type;
+                        }
+                    }
+                }
             }
-        };
+            return type;
+        }();
     });
 
     window.location.href.replace(/\/.*\/(.*?)\.html/, function (sth, filename) {
         yangaiche(load)('./js/default/' + filename + '.js');
-        alert(yangaiche(browser_type)());
+        //yangaiche(load)('./js/default/' + filename + '.js');
+        //alert(yangaiche(browser_type));
+        //alert(yangaiche(browser_type));
     });
 }(window.jQuery);
