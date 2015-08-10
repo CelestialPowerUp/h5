97f47279ccb8e62c448912dd0734271c9aa1f517
caols <caols@y-car.com.cn>
2015-08-10 15:50:09 +0800

    订单对象

--- webapp/js/default/order.js
+++ webapp/js/default/order.js
@@ -1,5 +1,32 @@
 yangaiche(sys.load_default_module)('repository', {});
 
-app.order = {
-
+ls.order = {
+    touch: 'order_touch',
+    update: 'order_update',
+    clear: 'order_clear',
+    order_info: 'order_info'
 };
+
+yangaiche(ls.order.touch, function () {
+    return function () {
+        var storage = yangaiche(sys.local_storage);
+        var order = storage.get(ls.order.order_info);
+        if (!yangaiche(sys.exist)(order)) {
+            storage.set(ls.order.order_info, {});
+            return {};
+        }
+        return order;
+    };
+});
+
+yangaiche(ls.order.update, function () {
+    return function (order) {
+        yangaiche(sys.local_storage).set(ls.order.order_info, order);
+    };
+});
+
+yangaiche(ls.order.clear, function () {
+    return function () {
+        yangaiche(sys.local_storage).remove(ls.order.order_info);
+    };
+});
