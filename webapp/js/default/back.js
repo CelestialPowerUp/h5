da89136fb4fa95bf09eed413b6caad2696c51439
caols <caols@y-car.com.cn>
2015-08-10 15:39:56 +0800

    提取repository文件，定义顶层模块，但是不写内容，内容由模块自身维护。

--- webapp/js/default/back.js
+++ webapp/js/default/back.js
@@ -1,23 +1,25 @@
 yangaiche(sys.load_default_module)('repository', {});
 
 ls.back = {
-    module_name: 'back',
+    go_back_to_reload: 'go_back_to_reload',
+    check_reload_cmd: 'check_reload_cmd',
     to_reload: 'to_reload',
     to_reload_value: 'T'
 };
 
-yangaiche(ls.back.module_name, function () {
-    return {
-        go_back_to_reload: function () {
-            yangaiche(sys.local_storage).set(ls.back.to_reload, ls.back.to_reload_value);
-            window.history.back();
-        },
-        check_reload_cmd: function () {
-            var storage = yangaiche(sys.local_storage);
-            if (ls.back.to_reload_value === storage.get(ls.back.to_reload)) {
-                storage.remove(ls.back.to_reload);
-                window.location.reload();
-            }
+yangaiche(ls.back.go_back_to_reload, function () {
+    return function () {
+        yangaiche(sys.local_storage).set(ls.back.to_reload, ls.back.to_reload_value);
+        window.history.back();
+    };
+});
+
+yangaiche(ls.back.check_reload_cmd, function () {
+    return function () {
+        var storage = yangaiche(sys.local_storage);
+        if (ls.back.to_reload_value === storage.get(ls.back.to_reload)) {
+            storage.remove(ls.back.to_reload);
+            window.location.reload();
         }
     };
 });
--- webapp/js/default/openid.js
+++ webapp/js/default/openid.js
@@ -2,25 +2,30 @@ yangaiche(sys.load_default_module)('repository', {});
 yangaiche(ls.load_default_module)('back', {});
 
 ls.openid = {
-    module_name: 'openid',
+    login_by_opencode: 'login_by_opencode',
+    before_login: 'before_login',
+    after_login: 'after_login',
     open_id: 'open_id',
     page_before_login: 'page_before_login'
 };
 
-yangaiche(ls.openid.module_name, function () {
-    return {
-        login_by_opencode: function () {
-            var now_page = window.location.href;
-            yangaiche(sys.local_storage).set(ls.openid.page_before_login, now_page);
-            window.location.href = "./open_id.html";
-        },
-        before_login: function () {
-            var url = yangaiche(sys.local_storage).get(ls.openid.page_before_login);
-            window.history.replaceState(null, null, url);
-        },
-        after_login: function () {
-            var back = yangaiche(ls.back.module_name);
-            back.go_back_to_reload();
-        }
+yangaiche(ls.openid.login_by_opencode, function () {
+    return function () {
+        var now_page = window.location.href;
+        yangaiche(sys.local_storage).set(ls.openid.page_before_login, now_page);
+        window.location.href = "./open_id.html";
+    };
+});
+
+yangaiche(ls.openid.before_login, function () {
+    return function () {
+        var url = yangaiche(sys.local_storage).get(ls.openid.page_before_login);
+        window.history.replaceState(null, null, url);
+    };
+});
+
+yangaiche(ls.openid.after_login, function () {
+    return function () {
+        yangaiche(ls.back.go_back_to_reload)();
     };
 });
