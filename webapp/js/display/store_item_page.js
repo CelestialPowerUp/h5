f6d25ae8bb592aab198085175063756b4dec7bc4
caols <caols@y-car.com.cn>
2015-08-11 18:36:16 +0800

    评论展示

--- webapp/js/display/store_item_page.js
+++ webapp/js/display/store_item_page.js
@@ -11,17 +11,17 @@
 
             function make_array(size) {
                 var array = [];
-                for (var i= 0; i < size; i++) {
+                for (var i = 0; i < size; i++) {
                     array.push(i);
                 }
                 return array;
             }
 
-            getReq('/v2/api/store/ware/detail.json?ware_id=' + getReqParam()['ware_id'], function(data) {
+            getReq('/v2/api/store/ware/detail.json?ware_id=' + getReqParam()['ware_id'], function (data) {
 
                 getStore().set('store_item_spec', data);
 
-                data['cover_img']['raw_url'] = data['cover_img']['raw_url'] + '?imageView2/3/w/'+ parseInt(device_width) +'/h/'+parseInt(device_width / 16 * 9)+'/interlace/1';
+                data['cover_img']['raw_url'] = data['cover_img']['raw_url'] + '?imageView2/3/w/' + parseInt(device_width) + '/h/' + parseInt(device_width / 16 * 9) + '/interlace/1';
 
                 var tpl = Handlebars.compile(t("#store_item_page_tpl").text());
                 t('body').prepend(tpl(data));
@@ -32,84 +32,75 @@
                 //var data = JSON.parse(raw_data)['data'];
 
                 var product_ids = '';
-                t.each(data['ware_products'], function(i, wp) {
+                t.each(data['ware_products'], function (i, wp) {
                     if (i !== 0) {
                         product_ids += ',';
                     }
                     product_ids += wp['product_id'];
                 });
 
-                var page = 1, total_page = 0, page_size = 10;
+                var page = 1, total_size = 0, real_total_size = 0, page_size = 2;
 
-                getReq('/v2/api/order/service_comment/page_list.json?page='+page+'&page_size='+page_size+'&product_ids=' + product_ids, function(comment_data) {
-                    t.each(comment_data['items'], function(i, d) {
+                function load_suc(data, callback) {
+                    t.each(data['items'], function (i, d) {
                         d['order_rating'] = make_array(d['service_rating']);
                         d['keeper_rating'] = make_array(d['keeper_rating']);
-                        d['create_time'] = d['create_time'].substr(0, (4+2+1+2+1));
+                        d['create_time'] = d['create_time'].substr(0, (4 + 2 + 1 + 2 + 1));
                     });
 
-                    if (comment_data.length > 0) {
+                    if (data['items'].length > 0) {
                         var tpl = Handlebars.compile(t("#store_item_comment_tpl").text());
-                        t('#store-item-comments').empty().html(tpl(comment_data['items']));
+                        t('#store-item-comments ul').append(tpl(data['items']));
 
-                        var startY;
-                        page = parseInt(comment_data['cur_page']) + 1;
-                        total_page = parseInt(comment_data['total_page']);
+                        page = parseInt(data['cur_page']) + 1;
+                        total_size = parseInt(data['total_size']);
+                        real_total_size += data['items'].length;
 
-                        t('body').hammer().on('panstart', function(e) {
-                            startY = e['gesture']['pointers'][0]['pageY'];
-                        });
-
-                        t('body').hammer().on('panend', function(e) {
-                            var endY = e['gesture']['pointers'][0]['pageY'];
-                            if (endY > startY) {
-                                getReq('/v2/api/order/service_comment/page_list.json?total_page='+total_page+'&page='+page+'&page_size='+page_size+'&product_ids=' + product_ids, function(comment_data) {
-                                    t.each(comment_data, function(i, d) {
-                                        d['order_rating'] = make_array(d['service_rating']);
-                                        d['keeper_rating'] = make_array(d['keeper_rating']);
-                                        d['create_time'] = d['create_time'].substr(0, (4+2+1+2+1));
-                                    });
-
-                                    if (comment_data.length > 0) {
-                                        var tpl = Handlebars.compile(t("#store_item_comment_tpl").text());
-                                        t('#store-item-comments').empty().html(tpl(comment_data));
-
-                                        page = parseInt(comment_data['cur_page']) + 1;
-                                        total_page = parseInt(comment_data['total_page']);
-                                    } else {
-                                        show_msg('没有更多评论了! ');
-                                        return;
-                                    }
-
-                                    var total_height = 0;
-                                    t.each(t('#store-item-comments ul li'), function(i, l) {
-                                        var list = t(l);
-                                        var height = (list.children('p').eq(0).height() + 70 * 2);
-                                        total_height += height;
-                                        list.css('height', height + 'px');
-                                    });
-                                    t('#store-item-comments ul').height(total_height);
-                                }, function(error) {
-                                    show_msg(error['message']);
-                                });
-                            }
-                            startY = 36500;
-                        });
+                        if (callback !== undefined && callback !== null) {
+                            callback();
+                        }
                     }
 
                     var total_height = 0;
-                    t.each(t('#store-item-comments ul li'), function(i, l) {
+                    t.each(t('#store-item-comments ul li'), function (i, l) {
                         var list = t(l);
                         var height = (list.children('p').eq(0).height() + 70 * 2);
                         total_height += height;
                         list.css('height', height + 'px');
                     });
                     t('#store-item-comments ul').height(total_height);
-                }, function(error) {
+                }
+
+                function load_more() {
+                    getReq('/v2/api/order/service_comment/page_list.json?total_size=' + total_size + '&page=' + page + '&page_size=' + page_size + '&product_ids=' + product_ids, function (comment_data) {
+                        load_suc(comment_data);
+                    }, function (error) {
+                        show_msg(error['message']);
+                    });
+                }
+
+                getReq('/v2/api/order/service_comment/page_list.json?page=' + page + '&page_size=' + page_size + '&product_ids=' + product_ids, function (comment_data) {
+                    load_suc(comment_data, function () {
+                        t("body").hammer().on('panend', function () {
+                            var $this = t(this),
+                                viewH = t(window).height(),//可见高度
+                                contentH = $this.get(0).scrollHeight,//内容高度
+                                scrollTop = $this.scrollTop();//滚动高度
+                            if (scrollTop / (contentH - viewH) >= 0.95) { //快到达底部时,加载新内容
+                                // 这里加载数据..
+                                if (real_total_size === total_size) {
+                                    show_msg('没有更多评论了! ');
+                                } else {
+                                    setTimeout(load_more, 1);
+                                }
+                            }
+                        });
+                    });
+                }, function (error) {
                     show_msg(error['message']);
                 });
 
-            }, function(error) {
+            }, function (error) {
                 show_msg(error['message']);
             });
 
