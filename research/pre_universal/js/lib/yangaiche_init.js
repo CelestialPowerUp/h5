d8d5bcc4fb5ee602c4e5dca4c50d7664edeb1bc3
caols <caols@y-car.com.cn>
2015-08-11 09:23:00 +0800

    删掉research

--- research/pre_universal/js/lib/yangaiche_init.js
+++
@@ -1,5 +0,0 @@
-yangaiche(sys.load)('./js/lib/yangaiche_module.js', {});
-
-window.location.href.replace(/\/.*\/(.*?)\.html/, function (sth, filename) {
-    yangaiche(sys.load_module)(filename + '_init');
-});
