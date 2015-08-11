b12d6fcfd6335017d32d49b208568fe8c536b2b7
caols <caols@y-car.com.cn>
2015-08-11 15:47:36 +0800

    发布脚本，本地fis后文件

--- deploy_tools/h5_develop.sh
+++ deploy_tools/h5_develop.sh
@@ -6,8 +6,8 @@ make_dir="h5-"${config}
 
 PATH=$PATH:./libs/
 
-bash _make_h5.sh ${local_dir} ${config} ${make_dir}
+bash _make_h5.sh ${local_dir} ${config} ${make_dir} &&
 
-# bash _ui.sh 123.59.52.186 yangaiche@test766 "/data/apps" ${local_dir}/${make_dir} &&
+bash _ui.sh 123.59.52.186 yangaiche@test766 "/data/apps" ${local_dir}/${make_dir} &&
 
-# bash _git.sh ${local_dir} ${config}
+bash _git.sh ${local_dir} ${config}
