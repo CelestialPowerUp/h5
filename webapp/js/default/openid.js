1469e4597764ef02630511c1414f440a1338fb84
caols <caols@y-car.com.cn>
2015-08-11 11:23:40 +0800

    openid全套

--- webapp/js/default/openid.js
+++ webapp/js/default/openid.js
@@ -1,12 +1,17 @@
 yangaiche(sys.load_default_module)('repository', {});
-yangaiche(ls.load_default_module)('back', {});
+yangaiche(sys.load_default_module)('back', {});
+yangaiche(sys.load_default_module)('env', {});
 
 ls.openid = {
     login_by_opencode: 'login_by_opencode',
     show_login_win: 'show_login_win',
     after_login: 'after_login',
+    get_redirect_uri: 'get_redirect_uri',
     
     open_id: 'open_id',
+    user_gender: 'user_gender',
+    user_mobile: 'user_mobile',
+    user_real_name: 'user_real_name',
     page_before_login: 'page_before_login'
 };
 
@@ -31,4 +36,10 @@ yangaiche(ls.openid.after_login, function () {
     return function (num) {
         yangaiche(ls.back.go_back_to_reload)(num);
     };
+});
+
+yangaiche(ls.openid.get_redirect_uri, function () {
+    return function () {
+        return 'http://' + yangaiche(app.env.get_host) + 'open_id.html';
+    }();
 });
--- webapp/js/xiaomi/open_id_init.js
+++ webapp/js/xiaomi/open_id_init.js
@@ -7,29 +7,30 @@ yangaiche(sys.load_default_module)('user', {});
 app.open_id_init = {};
 
 app.open_id_init.situation = 'production';
-app.open_id_init.appId = 'wxb78dc0eb87da0df9';
+app.open_id_init.appId = '2882303761517266846';
 function fn1() {
     app.open_id_init.situation = 'test';
-    app.open_id_init.appId = 'wx6569a515e0c3e346';
+    app.open_id_init.appId = '2882303761517356288';
 }
 function fn2() {
     app.open_id_init.situation = 'production';
-    app.open_id_init.appId = 'wxb78dc0eb87da0df9';
+    app.open_id_init.appId = '2882303761517266846';
 }
 yangaiche(app.env.do_sth)({
     dev: fn1,
-    staging: fn1,
+    staging: fn2,
     product: fn2
 });
 
 app.open_id_init.redirect_uri = encodeURIComponent(yangaiche(ls.openid.get_redirect_uri));
-app.open_id_init.snsapi = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=' + app.open_id_init.appId + '&redirect_uri=' + app.open_id_init.redirect_uri + '&response_type=code&scope=snsapi_base#wechat_redirect';
-
-app.open_id_init.reqParam = yangaiche(app.url_parameter);
+app.open_id_init.snsapi = 'https://account.xiaomi.com/oauth2/authorize?client_id=' + app.open_id_init.appId + '&response_type=token&redirect_uri=' + app.open_id_init.redirect_uri;
+app.open_id_init.reqParam = yangaiche(app.hash_parameter);
 app.open_id_init.to_snsapi = true;
-if (reqParam['code']) {
-    // TODO : get还是post啦?
-    yangaiche(app.http.get_request)("/v1/api/login_by_wx_code.json?code=" + app.open_id_init.reqParam['code'] + "&situation=" + app.open_id_init.situation, {}, function (data) {
+app.open_id_init.accessToken = app.open_id_init.reqParam['access_token'];//小米返回的access_token值
+//app.open_id_init.macKey = app.open_id_init.reqParam['mac_key'];//获取小米userId需要用到的macKey
+//BUT : 目前没用到macKey好好地运作着...
+if (app.open_id_init.accessToken/* && macKey*/) {
+    yangaiche(app.http.post_request)("/v1/api/login_by_xiaomi_openid.json?code=" + app.open_id_init.accessToken + "&situation=" + app.open_id_init.situation, {}, function (data) {
         yangaiche(ls.user.set)(data);
         yangaiche(ls.openid.after_login)(-3);
     }, function (data) {
