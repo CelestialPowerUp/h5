3e23b98fd023e11d6197eb5214687d551895aa52
caols <caols@y-car.com.cn>
2015-08-11 10:21:07 +0800

    页面内的变量全部放到app对象里面，为了混淆

--- webapp/js/weixin/open_id_init.js
+++ webapp/js/weixin/open_id_init.js
@@ -4,37 +4,43 @@ yangaiche(sys.load_default_module)('http', {});
 yangaiche(sys.load_default_module)('openid', {});
 yangaiche(sys.load_default_module)('user', {});
 
-var situation, appId;
+app.open_id_init = {};
+
+app.open_id_init.situation = 'production';
+app.open_id_init.appId = 'wxb78dc0eb87da0df9';
 function fn1() {
-	situation = 'test';
-	appId = 'wx6569a515e0c3e346';
+    app.open_id_init.situation = 'test';
+    app.open_id_init.appId = 'wx6569a515e0c3e346';
 }
 function fn2() {
-	situation = 'production';
-	appId = 'wxb78dc0eb87da0df9';
+    app.open_id_init.situation = 'production';
+    app.open_id_init.appId = 'wxb78dc0eb87da0df9';
 }
 yangaiche(app.env.do_sth)({
-	dev: fn1,
-	staging: fn1,
-	production: fn2
+    dev: fn1,
+    staging: fn1,
+    product: fn2
 });
 
-var snsapi = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=' + appId + '&redirect_uri=http%3A%2F%2F' + host + 'wechat%2Fopen_id.html&response_type=code&scope=snsapi_base#wechat_redirect';
+app.open_id_init.redirect_uri = encodeURIComponent('http://' + yangaiche(app.env.get_host) + 'open_id.html');
+app.open_id_init.snsapi = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=' + app.open_id_init.appId + '&redirect_uri=' + app.open_id_init.redirect_uri + '&response_type=code&scope=snsapi_base#wechat_redirect';
 
-var reqParam = yangaiche(app.url_parameter), to_snsapi = true;
+app.open_id_init.reqParam = yangaiche(app.url_parameter);
+app.open_id_init.to_snsapi = true;
 if (reqParam['code']) {
-    yangaiche(app.http.get_req)("login_by_wx_code.json?code="+reqParam['code']+"&situation="+situation,{},function(data){
+    // TODO : get还是post啦?
+    yangaiche(app.http.get_request)("login_by_wx_code.json?code=" + app.open_id_init.reqParam['code'] + "&situation=" + app.open_id_init.situation, {}, function (data) {
         yangaiche(ls.user.set)(data);
-        after_login();
-    },function(data){
-        getStore().set("open_id",data.data);
-        if(data && data['code'] == '10007'){
+        yangaiche(ls.openid.after_login)(-3);
+    }, function (data) {
+        getStore().set("open_id", data.data);
+        if (data && data['code'] == '10007') {
             show_login_win();
         }
     });
-    to_snsapi = false;
+    app.open_id_init.to_snsapi = false;
 }
 
-if (to_snsapi) {
-    window.location.href = snsapi;
+if (app.open_id_init.to_snsapi) {
+    window.location.href = app.open_id_init.snsapi;
 }
