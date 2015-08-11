e84e73f807ceb6c0030883fc3a4c6bb0646b8dc9
caols <caols@y-car.com.cn>
2015-08-11 15:28:18 +0800

    读取evn.json，作为运行时host

--- webapp/js/default/env.js
+++ webapp/js/default/env.js
@@ -1,51 +1,45 @@
 yangaiche(sys.load_default_module)('repository', {});
 
 app.env = {
-	get: 'get_env',
-	do_sth: 'do_sth_by_env',
-	get_host: 'get_host'
+    get: 'get_env',
+    do_sth: 'do_sth_by_env',
+    get_host: 'get_host'
 };
 
-yangaiche(app.env.get, function() {
-	return function() {
-		var cfg = $.ajax({
-	        url: './map/env.json',
-	        cache: true,
-	        async: false,
-	        dataType: 'json'
-	    });
-	    return cfg['responseJSON']['thisis'];
-	}();
+yangaiche(app.env.get, function () {
+    return function () {
+        var cfg = $.ajax({
+            url: './map/env.json',
+            cache: true,
+            async: false,
+            dataType: 'json'
+        });
+        return cfg['responseJSON']['thisis'];
+    }();
 });
 
-yangaiche(app.env.do_sth, function() {
-	return function(sth, params) {
-		sth[yangaiche(app.env.get)](params);
-	};
+yangaiche(app.env.do_sth, function () {
+    return function (sth, params) {
+        sth[yangaiche(app.env.get)](params);
+    };
 });
 
 yangaiche(app.env.get_host, function () {
-	// 如需转义，请使用encodeURIComponent方法，对应的方法是decodeURIComponent
-	return function () {
-		var host = null;
+    // 如需转义，请使用encodeURIComponent方法，对应的方法是decodeURIComponent
+    return function () {
+        var cfg = $.ajax({
+            url: './map/env.json',
+            cache: true,
+            async: false,
+            dataType: 'json'
+        });
+        var host = cfg['responseJSON']['domain'] + '/';
 
-		yangaiche(app.env.do_sth)({
-			dev: function() {
-				host = 'dev.yangaiche.com/developer/';
-			},
-			staging: function() {
-				host = 'dev.yangaiche.com/stage/';
-			},
-			product: function() {
-				host = 'pay.yangaiche.com/';
-			}
-		});
+        if (host.indexOf(window.location.host) >= 0) {
+            return host;
+        }
 
-		if (host.indexOf(window.location.host) >= 0) {
-			return host;
-		}
-
-		host = window.location.host + '/h5/';
-		return host;
-	}();
+        host = window.location.host + '/h5/';
+        return host;
+    }();
 });
