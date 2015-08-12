889d3197337f31c790ccc2b72db4aa048f8254c4
caols <caols@y-car.com.cn>
2015-08-12 10:32:53 +0800

    提交发布脚本

--- deploy_tools/libs/_make_h5.sh
+++ deploy_tools/libs/_make_h5.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-echo making fis configs &&
-bash _make_fis_configs.sh $1 &&
-echo "done" &&
-
 echo $1-\>$2-\>$3 &&
 
+echo "making fis configs." &&
+bash _make_fis_configs.sh $1 &&
+echo "done." &&
+
+echo "copy file to tmp folder.." &&
 mkdir -p $1/tmp/ &&
 cp -a $1/webapp/* $1/tmp/ &&
 cp -a $1/enviroment/$2/* $1/tmp/map/ &&
+echo "done.." &&
 
 mkdir -p $1/$3 &&
 fis release -comp -r $1/tmp/ -f $1/"fis-cfgs"/"fis-"$2"-conf.js" -d $1/$3 -D &&
 
-echo "done..."
+echo "clean tmp folder..." &&
+rm -rf $1/tmp/ &&
+echo "done..." &&
+
+echo "well done!!!"
