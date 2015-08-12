83c61218184d6a670ebeda0465cf8109510dac7d
CaoLiSheng <caols@y-car.com.cn>
2015-08-12 14:36:40 +0800

    HTTP header settings

--- webapp/js/default/http.js
+++ webapp/js/default/http.js
@@ -39,8 +39,8 @@ function get_real_url(url) {
 }
 
 function default_header(request) {
-    request.setRequestHeader("Accept-Encoding", 'gzip');
-    request.setRequestHeader("API-Client-Device-Type", yangaiche(sys.browser_type));
+    //request.setRequestHeader("Accept-Encoding", 'gzip');
+    request.setRequestHeader("API-Client-Device-Type", yangaiche(sys.browser_type).type);
     yangaiche(ls.user.if_exist)(function(user) {
         request.setRequestHeader("API-Access-Token", user.token);
     });
