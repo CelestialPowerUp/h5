3752662537c07d54597d27645f78550db7aa9bb6
CaoLiSheng <caols@y-car.com.cn>
2015-08-14 16:06:00 +0800

    预防隐藏bug

--- webapp/js/default/back.js
+++ webapp/js/default/back.js
@@ -39,6 +39,11 @@ yangaiche(ls.back.set_back_to_his, function() {
 yangaiche(ls.back.get_parent_of, function() {
     var storage = yangaiche(sys.local_storage);
     return function(url) {
-        return storage.get(url);
+        var referer = storage.get(url);
+        if (yangaiche(sys.exist)(referer)) {
+            return referer;
+        } else {
+            return null;
+        }
     };
 });
