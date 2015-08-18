5073735cd3391a7d95f147553d6be0e000db31dd
CaoLiSheng <caols@y-car.com.cn>
2015-08-18 14:33:02 +0800

    取商品通过service type

--- webapp/js/default/simple_service_product_info.js
+++ webapp/js/default/simple_service_product_info.js
@@ -5,9 +5,22 @@ yangaiche(sys.load_default_module)('order');
 yangaiche(sys.load_default_module)('products');
 yangaiche(sys.load_default_module)('back');
 
+app.simple_service_products = {
+    config: {
+        car_beauty_service_product_info: 2,
+        renewal_service_product_info: 3,
+        home_testing_service_product_info: 4,
+        set_loss_service_product_info: 5,
+        vehicle_exam_service_product_info: 13
+    }
+};
+
 yangaiche(sys.init)(function (t) {
-    var order = yangaiche(ls.order.touch)();
-    yangaiche(app.http.get_request)('/v1/api/products.json?service_type=1&supplier_id=' + order.supplier_id + '&car_model_type=' + order.car_model_type, function (data) {
+    app.simple_service_products.key = window.location.href.match(/\/.*\/(.*?)\.html/)[1];
+
+    var order = yangaiche(ls.order.touch)(),
+        service_type = app.simple_service_products.config[app.simple_service_products.key];
+    yangaiche(app.http.get_request)('/v1/api/products.json?service_type=' + service_type + '&supplier_id=' + order.supplier_id + '&car_model_type=' + order.car_model_type, function (data) {
         var total_price = yangaiche(ls.products.calculate)(data['required_products']);
         t('#total_price').html('¥' + total_price);
         yangaiche(ls.products.set)(data['required_products']);
