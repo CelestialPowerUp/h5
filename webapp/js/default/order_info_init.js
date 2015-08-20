fe26a09637d40e24b8231ee5bb965122bc7a3a16
CaoLiSheng <caols@y-car.com.cn>
2015-08-20 12:22:53 +0800

    增项第二弹，half

--- webapp/js/default/order_info_init.js
+++ webapp/js/default/order_info_init.js
@@ -14,30 +14,49 @@ yangaiche(sys.init)(function (t) {
         show_msg = yangaiche(app.show_msg.show);
 
     var parse_data = function (order) {
-        var template = Handlebars.compile(t("#user_info_tpl").html());
-        t("#user_info_view").html(template(order));
-
-        var product_template = Handlebars.compile(t("#product_info_tpl").html());
-        t("#products_view").html(product_template(order.products));
-
-        var paid_template = Handlebars.compile(t("#paid_tpl").html());
-        t("#paid_view").html(paid_template(order));
-
-        order.pay_type_info = yangaiche(app.pay.to_pay_type_info)(order);
-        var paid_type_template = Handlebars.compile(t("#paid_type_tpl").html());
-        t("#paid_type_view").html(paid_type_template(order));
+        console.log(order);
+        var template = Handlebars.compile(t("#order_info_tpl").html());
+        t("body").prepend(template(order));
     };
 
     var reqParams = yangaiche(app.url_parameter);
     if (yangaiche(sys.exist)(reqParams['order_id'])) {//有参数，查看页面
         getReq("/v2/api/orders.json?order_id=" + reqParams['order_id'], function (order) {
-            order.car_number = order.car.licence.province + order.car.licence.number;
-            order.contact_name = order.client_basic.name;
-            order.phone_number = order.client_basic.phone_number;
-            order.location = order.client_basic.location;
+            order['client_basic'].car_number = order.car.licence.province + order.car.licence.number;
             if (order['coupon_price'] > 0) {
                 order['coupon'] = {value: order['coupon_price'].toFixed(2)};
             }
+
+            order.to_select = null;
+            order.self_items = [];
+
+            t.each(order['products'], function(i, p) {
+                var selection_mode = p['selection_mode'];
+                if (selection_mode === 1) {
+                    order.self_items.push(p);
+                } else if (selection_mode === 2) {
+                    if (yangaiche(sys.exist)(order.to_select)) {
+                        order.to_select = {
+                            selected_items: [],
+                            unselected_items: []
+                        };
+                    }
+                    order.to_select.unselected_items.push(p);
+                } else if (selection_mode !== 5) {
+                    if (yangaiche(sys.exist)(order.to_select)) {
+                        order.to_select = {
+                            selected_items: [],
+                            unselected_items: []
+                        };
+                    }
+                    order.to_select.selected_items.push(p);
+                }
+            });
+
+            if (order['notPaidPrice'] > 0) {
+                order.not_paid = {not_paid_price: order['not_paid_price'] || order['notPaidPrice']};
+            }
+            order.paid_price = order['paid_price'] || order['paidPrice'];
 
             parse_data(order);
 
