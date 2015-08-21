e7a1663dfd5d3e28f03c5cda9899b72cfed82987
CaoLiSheng <caols@y-car.com.cn>
2015-08-21 12:10:30 +0800

    安卓需计算device pixel ratio，iPhone不用，呵呵呵

--- webapp/js/lib/yangaiche_init.js
+++ webapp/js/lib/yangaiche_init.js
@@ -1,8 +1,8 @@
 yangaiche(sys.load)('js/lib/yangaiche_module.js', {});
 yangaiche(sys.load_default_module)('repository', {});
 
-yangaiche(sys.load_default_module)('viewport', {});
-yangaiche(app.viewport.set)();
+//yangaiche(sys.load_default_module)('viewport', {});
+//yangaiche(app.viewport.set)();
 
 window.location.href.replace(/\/.*\/(.*?)\.html/, function (sth, filename) {
     yangaiche(sys.load_module)(filename + '_init');
