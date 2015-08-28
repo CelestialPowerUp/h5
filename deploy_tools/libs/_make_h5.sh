daef2f225941589d69a851307d4545ca25d150f8
CaoLiSheng <caols@y-car.com.cn>
2015-08-28 10:04:02 +0800

    提交发布脚本

--- deploy_tools/libs/_make_h5.sh
+++ deploy_tools/libs/_make_h5.sh
@@ -18,8 +18,12 @@ fi
 mkdir -p $1/$3 &&
 fis release -comp -r $1/tmp/ -f $1/"fis-cfgs"/"fis-"$2"-conf.js" -d $1/$3 -D &&
 
-echo "clean tmp folder..." &&
-rm -rf $1/tmp/ &&
+echo "copy json configs..." &&
+bash _cp_json.sh $1/enviroment/$2 $1/platform/normal $1/$3 &&
 echo "done..." &&
+
+echo "clean tmp folder...." &&
+rm -rf $1/tmp/ &&
+echo "done...." &&
 
 echo "well done!!!"
