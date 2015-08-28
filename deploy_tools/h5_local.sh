47d6d58a000e6692ba0f7275f34c977eceb7fdad
CaoLiSheng <caols@y-car.com.cn>
2015-08-28 16:52:34 +0800

    提交发布脚本

--- deploy_tools/h5_local.sh
+++ deploy_tools/h5_local.sh
@@ -7,7 +7,3 @@ make_dir="h5-"${config}
 PATH=$PATH:./libs/
 
 bash _make_h5.sh ${local_dir} ${config} ${make_dir}
-
-# bash _ui.sh 123.59.52.186 yangaiche@test766 "/data/apps" ${local_dir}/${make_dir} &&
-
-# bash _git.sh ${local_dir} ${config}
--- deploy_tools/libs/_ui.sh
+++ deploy_tools/libs/_ui.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
+local_dir="$1"
+ssh_shortcut="$2"
+remote_path="$3"
 
-#git stash
-#git checkout feature_backend_ui &&
-#git pull github_core  &&
-echo $4 $1:$3
-rsync -avc $4 --rsh="sshpass -p $2 ssh -o StrictHostKeyChecking=no -l root"  $1:$3
- 
-#git stash pop
+rsync -avc --progress ${local_dir} ${ssh_shortcut}:${remote_path}
