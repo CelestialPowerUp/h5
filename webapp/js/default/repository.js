920568bf0a6d4510a6f3acc738e71060a83a56b6
CaoLiSheng <caols@y-car.com.cn>
2015-08-31 15:02:28 +0800

    button显示逻辑

--- webapp/js/default/repository.js
+++ webapp/js/default/repository.js
@@ -7,7 +7,9 @@ var key = {
         submit_text_value2: '去支付',
         submit_text_value3: '立即预约',
         submit_text_value4: '去评价',
-        submit_text_value5: '已支付'
+        submit_text_value5: '已支付',
+        submit_text_value6: '已评价',
+        submit_text_value7: '无需支付'
     },
     goto: {
         car_list: 'car_list_goto'
