7919087513cff39f06d4b52691fcacf992fa6499
CaoLiSheng <caols@y-car.com.cn>
2015-09-01 16:16:02 +0800

    订单列表分页准备工作

--- webapp/js/default/order_list_init.js
+++ webapp/js/default/order_list_init.js
@@ -2,6 +2,7 @@ yangaiche(sys.load_default_module)('http', {});
 yangaiche(sys.load_default_module)('user', {});
 yangaiche(sys.load_default_module)('format', {});
 yangaiche(sys.load_default_module)('show_msg', {});
+yangaiche(sys.load_default_module)('paging');
 
 yangaiche(sys.init)(function (t) {
     var storage = yangaiche(sys.local_storage), now_scroll_top = 'now_scroll_top';
@@ -9,95 +10,95 @@ yangaiche(sys.init)(function (t) {
         storage.set(now_scroll_top, document.body.scrollTop);
     };
 
-    var progress = $.AMUI.progress;
-    progress.start();
-    progress.inc(0.5);
-    t('#nprogress .nprogress-bar').css('bottom', 'auto');
-    t('#nprogress .nprogress-bar').css('top', '0');
-
-    yangaiche(app.http.get_request)("/v3/api/orders.json?user_type=caruser&user_id=" + yangaiche(ls.user.touch)()[ls.user.user_id], function (data) {
-        var items = [];
-        for (var i = 0; i < data.length; i++) {
-            var item = {};
-            item.id = data[i].id;
-            item.car_number = data[i].car.licence.province + data[i].car.licence.number;
-            item.total_price = data[i].total_price;
-            item.place_time = yangaiche(app.format.time)(data[i].place_time);
-            item.order_status_value = data[i].order_status_value;
-            if (data[i]['order_status_key'].match(/complete|completed/)) {
-                item.comment_btn_text = key.submit_button.submit_text_value4;
-                item.gray_button = '';
-            } else {
-                item.gray_button = 'gray_button';
-                if ('evaluated' === data[i]['order_status_key']) {
-                    item.comment_btn_text = key.submit_button.submit_text_value6;
+    yangaiche(app.paging.setup)({
+        url_request: "/v3/api/orders.json?user_type=caruser&user_id=" + yangaiche(ls.user.touch)()[ls.user.user_id],
+        data_handler: function(data) {
+            var items = [];
+            for (var i = 0; i < data.length; i++) {
+                var item = {};
+                item.id = data[i].id;
+                item.car_number = data[i].car.licence.province + data[i].car.licence.number;
+                item.total_price = data[i].total_price;
+                item.place_time = yangaiche(app.format.time)(data[i].place_time);
+                item.order_status_value = data[i].order_status_value;
+                if (data[i]['order_status_key'].match(/complete|completed/)) {
+                    item.comment_btn_text = key.submit_button.submit_text_value4;
+                    item.gray_button = '';
                 } else {
-                    item.comment_btn_text = key.submit_button.submit_text_value8;
+                    item.gray_button = 'gray_button';
+                    if ('evaluated' === data[i]['order_status_key']) {
+                        item.comment_btn_text = key.submit_button.submit_text_value6;
+                    } else {
+                        item.comment_btn_text = key.submit_button.submit_text_value8;
+                    }
                 }
-            }
-            if (!data[i]['order_status_key'].match(/unconfirmed|confirmed/)) {
-                item.gray_button2 = 'gray_button';
-            }
-            items.push(item);
-        }
-        console.log(items);
-        var product_template = Handlebars.compile(t("#order_list_item_tpl").html());
-        t("#order_list_view").html(product_template(items));
-
-        var now_scroll_top_value = storage.get(now_scroll_top);
-        if (yangaiche(sys.exist)(now_scroll_top_value)) {
-            document.body.scrollTop = now_scroll_top_value;
-            storage.remove(now_scroll_top);
-        }
-
-        t('.order-comment-btn').bind('click', function () {
-            if (t(this).hasClass('gray_button')) {
-                return;
-            }
-
-            save_now_location();
-            yangaiche(ls.back.set_back_to_self)('order_comment.html?order_id=' + t(this).attr('data-rel'));
-        });
-
-        t('.order-flow-btn').bind('click', function () {
-
-            save_now_location();
-            yangaiche(ls.back.set_back_to_self)('order_flow.html?order_id=' + t(this).attr('data-rel'));
-        });
-
-        t('.order-cancel-btn').bind('click', function () {
-            if (t(this).hasClass('gray_button')) {
-                return;
-            }
-
-            save_now_location();
-
-            var order_id = t(this).attr('data-rel');
-            t('#my-confirm').modal({
-                relatedTarget: this,
-                onConfirm: function () {
-                    yangaiche(app.http.post_request)('/v1/api/order_status/update.json', {
-                        "order_id": order_id,
-                        "order_status": "cancelled"
-                    }, function (data) {
-                        yangaiche(app.show_msg.show)(data);
-                        window.location.reload();
-                    }, function (error) {
-                        yangaiche(app.show_msg.show)(error['message']);
-                    });
-                },
-                // closeOnConfirm: false,
-                onCancel: function () {
+                if (!data[i]['order_status_key'].match(/unconfirmed|confirmed/)) {
+                    item.gray_button2 = 'gray_button';
                 }
+                items.push(item);
+            }
+            console.log(items);
+            var product_template = Handlebars.compile(t("#order_list_item_tpl").html());
+            t("#order_list_view").append(product_template(items));
+
+            var now_scroll_top_value = storage.get(now_scroll_top);
+            if (yangaiche(sys.exist)(now_scroll_top_value)) {
+                document.body.scrollTop = now_scroll_top_value;
+                storage.remove(now_scroll_top);
+            }
+
+            t('.order-comment-btn').unbind('click');
+            t('.order-comment-btn').bind('click', function () {
+                if (t(this).hasClass('gray_button')) {
+                    return;
+                }
+
+                save_now_location();
+                yangaiche(ls.back.set_back_to_self)('order_comment.html?order_id=' + t(this).attr('data-rel'));
             });
-        });
 
-        t('.order-list-item-content').bind('click', function () {
+            t('.order-flow-btn').unbind('click');
+            t('.order-flow-btn').bind('click', function () {
 
-            save_now_location();
-            yangaiche(ls.back.set_back_to_self)('order_info.html?order_id=' + t(this).attr('data-rel'));
-        });
+                save_now_location();
+                yangaiche(ls.back.set_back_to_self)('order_flow.html?order_id=' + t(this).attr('data-rel'));
+            });
 
-        progress.done();
+            t('.order-cancel-btn').unbind('click');
+            t('.order-cancel-btn').bind('click', function () {
+                if (t(this).hasClass('gray_button')) {
+                    return;
+                }
+
+                save_now_location();
+
+                var order_id = t(this).attr('data-rel');
+                t('#my-confirm').modal({
+                    relatedTarget: this,
+                    onConfirm: function () {
+                        yangaiche(app.http.post_request)('/v1/api/order_status/update.json', {
+                            "order_id": order_id,
+                            "order_status": "cancelled"
+                        }, function (data) {
+                            yangaiche(app.show_msg.show)(data);
+                            window.location.reload();
+                        }, function (error) {
+                            yangaiche(app.show_msg.show)(error['message']);
+                        });
+                    },
+                    // closeOnConfirm: false,
+                    onCancel: function () {
+                    }
+                });
+            });
+
+            t('.order-list-item-content').unbind('click');
+            t('.order-list-item-content').bind('click', function () {
+
+                save_now_location();
+                yangaiche(ls.back.set_back_to_self)('order_info.html?order_id=' + t(this).attr('data-rel'));
+            });
+        }
     });
+
 });
