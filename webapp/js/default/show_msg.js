b0d0faa3678b2efd727d014c2053129805db872b
CaoLiSheng <caols@y-car.com.cn>
2015-09-02 10:52:20 +0800

    去掉alert的关闭按钮，内容字体设置34px，并且自动消失。

--- webapp/js/default/show_msg.js
+++ webapp/js/default/show_msg.js
@@ -7,7 +7,6 @@ app.show_msg = {
     html: '<div class="am-modal am-modal-no-btn" tabindex="-1" id="yac-modal">' +
     '<div class="am-modal-dialog">' +
     '<div class="am-modal-hd">养爱车提示' +
-    '<a href="javascript: void(0)" class="am-close am-close-spin" data-am-modal-close>&times;</a>' +
     '</div>' +
     '<div class="am-modal-bd">' +
     '{{msg_to_show}}' +
@@ -31,14 +30,25 @@ yangaiche(app.show_msg.init, function() {
 
 yangaiche(app.show_msg.show, function () {
     var tpl = Handlebars.compile(app.show_msg.html),
-        t = yangaiche(sys.$);
+        t = yangaiche(sys.$),
+        active = true;
     return function (msg) {
+        if (!active) {
+            return false;
+        }
+
         var text = tpl({msg_to_show: msg});
         console.log(text);
         t('#msg_wrapper').empty().html(text);
 
         var $modal = t('#yac-modal');
         $modal.modal();
+        active = false;
+
+        setTimeout(function() {
+            $modal.modal('close');
+            active = true;
+        }, 2000);
 
         //alert(msg);
     };
