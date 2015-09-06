14cdbbd1a3fbfa792279f5870fb1b51358ce0e3a
CaoLiSheng <caols@y-car.com.cn>
2015-09-06 13:36:23 +0800

    支付中openid参数

--- webapp/js/weixin/pay_param.js
+++ webapp/js/weixin/pay_param.js
@@ -3,9 +3,13 @@ yangaiche(sys.load_default_module)('user', {});
 
 app.pay.get_extra_param = 'get_pay_extra_param';
 
-yangaiche(app.pay.get_extra_param, function() {
-    return function(param) {
+yangaiche(app.pay.get_extra_param, function () {
+    return function (param) {
         param.channel = 'wx_pub';
-        param.extra = { open_id: yangaiche(sys.local_storage).get(ls.openid.open_id) || yangaiche(ls.user.touch)()[ls.user.openid] };
+        var openid = yangaiche(sys.local_storage).get(ls.openid.open_id);
+        if (!yangaiche(sys.exist)(openid) || openid === '') {
+            openid = yangaiche(ls.user.touch)()[ls.user.openid];
+        }
+        param.extra = {open_id: openid};
     };
 });
