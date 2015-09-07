c03d4254f0e53285bd2ddf608af261b7890ec586
CaoLiSheng <caols@y-car.com.cn>
2015-09-07 18:51:56 +0800

    copy的官方实例

--- webapp/js/yangaiche/store_init.js
+++ webapp/js/yangaiche/store_init.js
@@ -3,30 +3,22 @@ yangaiche(sys.load_default_module)('store_init');
 yangaiche(sys.init)(function(t) {
     function connectWebViewJavascriptBridge(callback) {
         if (window.WebViewJavascriptBridge) {
-            callback(WebViewJavascriptBridge);
+            callback(WebViewJavascriptBridge)
         } else {
             document.addEventListener('WebViewJavascriptBridgeReady', function() {
-                callback(WebViewJavascriptBridge);
+                callback(WebViewJavascriptBridge)
             }, false)
         }
     }
 
     connectWebViewJavascriptBridge(function(bridge) {
-
-        /* Init your app here */
-
         bridge.init(function(message, responseCallback) {
-            alert('Received message: ' + message);
-            if (responseCallback) {
-                responseCallback("Right back atcha")
-            }
-        });
-        //bridge.send('Hello from the javascript')
-        //bridge.send('Please respond to this', function responseCallback(responseData) {
-        //    console.log("Javascript got its response", responseData)
-        //})
+            alert('JS got a message' + JSON.stringify(message));
+            var data = { 'Javascript Responds':'Wee!' };
+            alert('JS responding with' + JSON.stringify(data));
+            responseCallback(data);
+        })
     });
-
 }, 0);
 
 yangaiche(sys.start)();
