5a8e5b20aa9ae9adedac3c081649896ba28b7614
CaoLiSheng <caols@y-car.com.cn>
2015-09-08 14:59:58 +0800

    可以通过原生共享用户信息了

--- webapp/js/yangaiche/store_init.js
+++ webapp/js/yangaiche/store_init.js
@@ -1,22 +1,24 @@
 yangaiche(sys.load_default_module)('store_init');
+yangaiche(sys.load_default_module)('user');
 
 yangaiche(sys.init)(function(t) {
     function connectWebViewJavascriptBridge(callback) {
         if (window.WebViewJavascriptBridge) {
-            callback(WebViewJavascriptBridge)
+            callback(WebViewJavascriptBridge);
         } else {
             document.addEventListener('WebViewJavascriptBridgeReady', function() {
-                callback(WebViewJavascriptBridge)
-            }, false)
+                callback(WebViewJavascriptBridge);
+            }, false);
         }
     }
 
     connectWebViewJavascriptBridge(function(bridge) {
+        var data = { 'Javascript Responds':'Wee!' };
         bridge.init(function(message, responseCallback) {
-            alert('JS got a message' + JSON.stringify(message));
-            var data = { 'Javascript Responds':'Wee!' };
-            alert('JS responding with' + JSON.stringify(data));
+            console.log('JS got a message' + message);
+            console.log('JS responding with' + JSON.stringify(data));
+            yangaiche(ls.user.set)(JSON.parse(message));
             responseCallback(data);
-        })
+        });
     });
 }, 0);
