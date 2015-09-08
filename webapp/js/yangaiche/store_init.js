81f43241eb13ecc318483c7baff785b7f412693f
CaoLiSheng <caols@y-car.com.cn>
2015-09-08 19:57:29 +0800

    tweak机制

--- webapp/js/yangaiche/store_init.js
+++ webapp/js/yangaiche/store_init.js
@@ -1,5 +1,6 @@
 yangaiche(sys.load_default_module)('store_init');
 yangaiche(sys.load_default_module)('user');
+yangaiche(sys.load_default_module)('http');
 yangaiche(sys.load_module)('ios/bridge');
 
 yangaiche(sys.init)(function (t) {
@@ -11,6 +12,20 @@ yangaiche(sys.init)(function (t) {
             yangaiche(ls.user.set)(JSON.parse(message));
             responseCallback(data);
         });
+
+        yangaiche(app.http.tweak)(function(type, request_type, url) {
+            var this_context = type + request_type + url,
+                accept_context = app.http.after_render + app.http.get + '/v2/api/store/home_ware_list.json';
+            if (this_context === accept_context) {
+                t('.home-page-products li').unbind('click').click(function () {
+                    var data = {type: 7, ware_id: t(this).attr('data-rel')};
+                    alert(JSON.stringify(data));
+                    bridge['callHandler']('route', data, function (responseData) {
+                        console.log('JS got response: ' + responseData);
+                    });
+                });
+            }
+        });
     });
 }, 0);
 
@@ -18,7 +33,7 @@ yangaiche(sys.init)(function (t) {
     yangaiche(app.bridge.connect)(function (bridge) {
         t('#services li a').unbind('click').click(function () {
             var array = t('#services li a'), index = 0, $this_data_rel = t(this).attr('data-rel');
-            t.each(array, function(i, a) {
+            t.each(array, function (i, a) {
                 if (t(a).attr('data-rel') === $this_data_rel) {
                     index = i;
                 }
