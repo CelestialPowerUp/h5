774f1d80f1926683c77d0b0338054c56c74e45dd
CaoLiSheng <caols@y-car.com.cn>
2015-09-08 15:49:51 +0800

    iOS 跳转初步探索

--- webapp/js/yangaiche/store_init.js
+++ webapp/js/yangaiche/store_init.js
@@ -1,24 +1,26 @@
 yangaiche(sys.load_default_module)('store_init');
 yangaiche(sys.load_default_module)('user');
+yangaiche(sys.load_module)('ios/bridge');
 
-yangaiche(sys.init)(function(t) {
-    function connectWebViewJavascriptBridge(callback) {
-        if (window.WebViewJavascriptBridge) {
-            callback(WebViewJavascriptBridge);
-        } else {
-            document.addEventListener('WebViewJavascriptBridgeReady', function() {
-                callback(WebViewJavascriptBridge);
-            }, false);
-        }
-    }
-
-    connectWebViewJavascriptBridge(function(bridge) {
-        var data = { 'Javascript Responds':'Wee!' };
-        bridge.init(function(message, responseCallback) {
-            console.log('JS got a message' + message);
-            console.log('JS responding with' + JSON.stringify(data));
+yangaiche(sys.init)(function (t) {
+    yangaiche(app.bridge.connect)(function (bridge) {
+        var data = {'Javascript Responds': 'Wee!'};
+        bridge.init(function (message, responseCallback) {
+            console.log('JS got a message: ' + message);
+            console.log('JS responding with: ' + JSON.stringify(data));
             yangaiche(ls.user.set)(JSON.parse(message));
             responseCallback(data);
         });
     });
 }, 0);
+
+yangaiche(sys.init)(function (t) {
+    yangaiche(app.bridge.connect)(function (bridge) {
+        t('#services li a').unbind('click').click(function () {
+            var data = {type: t(this).index() + 1};
+            bridge['callHandler']('route', data, function (responseData) {
+                console.log('JS got response: ' + responseData);
+            });
+        });
+    });
+});
