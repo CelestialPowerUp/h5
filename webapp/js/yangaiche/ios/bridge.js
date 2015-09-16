6bace12c7f00c0b7675e407cb20aaab3e0b629b1
CaoLiSheng <caols@y-car.com.cn>
2015-09-16 16:21:44 +0800

    打印信息

--- webapp/js/yangaiche/ios/bridge.js
+++ webapp/js/yangaiche/ios/bridge.js
@@ -5,9 +5,12 @@ app.bridge = {
 yangaiche(app.bridge.connect, function () {
     return function (callback) {
         if (window['WebViewJavascriptBridge']) {
+            alert("already " + JSON.stringify(window['WebViewJavascriptBridge']));
             callback(window['WebViewJavascriptBridge']);
         } else {
+            alert("wait for event " + JSON.stringify(window['WebViewJavascriptBridge']));
             document.addEventListener('WebViewJavascriptBridgeReady', function () {
+                alert("event " + JSON.stringify(window['WebViewJavascriptBridge']));
                 callback(window['WebViewJavascriptBridge']);
             }, false);
         }
