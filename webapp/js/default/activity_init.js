a42c589a0bcf3d7f490abf7bd615252cc60c476c
CaoLiSheng <caols@y-car.com.cn>
2015-09-16 11:10:47 +0800

    十分简化player端。

--- webapp/js/default/activity_init.js
+++ webapp/js/default/activity_init.js
@@ -1,3 +1,17 @@
-yangaiche(sys.init)(function () {
+yangaiche(sys.load_default_module)('http');
+yangaiche(sys.load_default_module)('parameter');
 
+yangaiche(sys.init)(function (t) {
+    yangaiche(app.http.get_request)('/v1/api/h5template/get_page_by_code.json?code=' + yangaiche(app.url_parameter)['code'], function (data) {
+        console.log(data);
+
+        t('#player').html(data['rendered_html']);
+
+        // init js suits
+        key.external_sale_configs = JSON.parse(data.js_suit.external_sale_configs);
+        console.log(key.external_sale_configs);
+        t.each(data.js_suit.js_suit, function (i, js_name) {
+            yangaiche(sys.load_module)(js_name);
+        });
+    });
 });
