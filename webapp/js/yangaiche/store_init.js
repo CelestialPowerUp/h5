a44df56168c7119364c2d8782c47e3944f29db69
CaoLiSheng <caols@y-car.com.cn>
2015-09-16 13:54:01 +0800

    打印测试代码

--- webapp/js/yangaiche/store_init.js
+++ webapp/js/yangaiche/store_init.js
@@ -8,19 +8,22 @@ yangaiche(sys.init)(function (t) {
     yangaiche(app.bridge.connect)(function (bridge) {
         var data = {'Javascript Responds': 'Wee!'};
         bridge.init(function (message, responseCallback) {
-            console.log('JS got a message: ' + message);
-            console.log('JS responding with: ' + JSON.stringify(data));
+            alert('JS got a message: ' + message);
+            alert('JS responding with: ' + JSON.stringify(data));
             yangaiche(ls.user.set)(JSON.parse(message));
             responseCallback(data);
         });
 
+        alert(1);
         yangaiche(app.http.tweak)(function (type, request_type, url) {
             var this_context = type + request_type + url,
                 accept_context = app.http.after_render + app.http.get + '/v2/api/store/home_ware_list.json',
                 accept_context2 = app.http.after_render + app.http.get + '/v2/api/store/banners.json';
+            alert(this_context);
             if (this_context === accept_context) {
                 t('.home-page-products li').unbind('click').click(function () {
                     var data = {type: 7, ware_id: t(this).attr('data-rel')};
+                    alert('JS responding with: ' + JSON.stringify(data));
                     bridge['callHandler']('route', data, function (responseData) {
                         console.log('JS got response: ' + responseData);
                     });
@@ -29,12 +32,14 @@ yangaiche(sys.init)(function (t) {
             if (this_context === accept_context2) {
                 t('#banner img').unbind('click').click(function () {
                     var data = {type: 0, web_url: t(t(this).parents()[0]).attr('data-rel')};
+                    alert('JS responding with: ' + JSON.stringify(data));
                     bridge['callHandler']('route', data, function (responseData) {
                         console.log('JS got response: ' + responseData);
                     });
                 });
             }
         });
+        alert(2);
     });
 
 }, 0);
@@ -49,6 +54,23 @@ yangaiche(sys.init)(function (t) {
                 }
             });
             var data = {type: index + 1};
+            alert('JS responding with: ' + JSON.stringify(data));
+            bridge['callHandler']('route', data, function (responseData) {
+                console.log('JS got response: ' + responseData);
+            });
+        });
+
+        t('.home-page-products li').unbind('click').click(function () {
+            var data = {type: 7, ware_id: t(this).attr('data-rel')};
+            alert('JS responding with: ' + JSON.stringify(data));
+            bridge['callHandler']('route', data, function (responseData) {
+                console.log('JS got response: ' + responseData);
+            });
+        });
+
+        t('#banner img').unbind('click').click(function () {
+            var data = {type: 0, web_url: t(t(this).parents()[0]).attr('data-rel')};
+            alert('JS responding with: ' + JSON.stringify(data));
             bridge['callHandler']('route', data, function (responseData) {
                 console.log('JS got response: ' + responseData);
             });
