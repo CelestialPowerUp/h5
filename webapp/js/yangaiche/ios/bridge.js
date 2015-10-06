7420cb1eed614acca2fa6366e4bd2f746f47890d
CaoLiSheng <caols@y-car.com.cn>
2015-10-06 20:01:29 +0800

    alert

--- webapp/js/yangaiche/ios/bridge.js
+++ webapp/js/yangaiche/ios/bridge.js
@@ -5,12 +5,12 @@ app.bridge = {
 yangaiche(app.bridge.connect, function () {
     return function (callback) {
         if (window['WebViewJavascriptBridge']) {
-            console.log("already " + JSON.stringify(window['WebViewJavascriptBridge']));
+            alert("already " + JSON.stringify(window['WebViewJavascriptBridge']));
             callback(window['WebViewJavascriptBridge']);
         } else {
-            console.log("wait for event " + JSON.stringify(window['WebViewJavascriptBridge']));
+            alert("wait for event " + JSON.stringify(window['WebViewJavascriptBridge']));
             document.addEventListener('WebViewJavascriptBridgeReady', function () {
-                console.log("event " + JSON.stringify(window['WebViewJavascriptBridge']));
+                alert("event " + JSON.stringify(window['WebViewJavascriptBridge']));
                 callback(window['WebViewJavascriptBridge']);
             }, false);
         }
