a9ae3587fc75456a177194522fc497e93d566de4
CaoLiSheng <caols@y-car.com.cn>
2015-10-10 19:17:53 +0800

    天空飘来6个字，快去下APP

--- webapp/js/default/coupon_got_init.js
+++ webapp/js/default/coupon_got_init.js
@@ -10,12 +10,12 @@ yangaiche(sys.init)(function (t) {
         t(this).removeClass('yac-coupon-got-btn-hover');
     }).click(function () {
         yangaiche(app.http.get_request)('/v2/api/store/home_ware_list.json', function (data) {
-            t.each(data, function(i, d) {
+            t.each(data, function (i, d) {
                 if ('超值优惠产品' !== d['ware_type_name']) {
                     return true;
                 }
 
-                t.each(d['ware_list'], function(j, ware) {
+                t.each(d['ware_list'], function (j, ware) {
                     if ('洗车打蜡' !== ware['ware_name']) {
                         return true;
                     }
@@ -31,4 +31,8 @@ yangaiche(sys.init)(function (t) {
     if (yangaiche(app.url_parameter)['done']) {
         yangaiche(app.show_msg.show)('这是你已经抢过的优惠券哦');
     }
+
+    t('#footer_close').click(function () {
+        t('#to_hide').hide();
+    });
 });
