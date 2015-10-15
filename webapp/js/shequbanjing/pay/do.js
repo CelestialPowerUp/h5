ba376b7bd37e19c83cd3cbfc3e2b8e62f332bc07
CaoLiSheng <caols@y-car.com.cn>
2015-10-15 15:22:46 +0800

    乐家园支付地址，未经测试版

--- webapp/js/shequbanjing/pay/do.js
+++ webapp/js/shequbanjing/pay/do.js
@@ -5,11 +5,11 @@ app.pay_do = {
     to_pay: 'app_pay_do_to_pay'
 };
 
-yangaiche(app.pay_do.to_pay, function() {
+yangaiche(app.pay_do.to_pay, function () {
     return function (param) {
         yangaiche(app.http.get_request)('/v1/api/radius/get_access_token.json', function (data) {
-            var token = '&access_token=' + data;
-            window.location.href = 'http://www.baidu.com/?order_id=' + param.order_id + token;
+            var token = 'access_token=' + data + '&';
+            window.location.href = 'http://oapi.shequbanjing.com/redirect?' + token + 'type=payment&order_id=' + param.order_id;
         }, function (error) {
             yangaiche(app.show_msg.show)(error['message']);
         });
