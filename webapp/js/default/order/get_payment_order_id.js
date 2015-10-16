165082d6414cc6c65034c69a1ab15cfb83d47d3d
CaoLiSheng <caols@y-car.com.cn>
2015-10-16 10:05:15 +0800

    支付的社区半径order_id

---
+++ webapp/js/default/order/get_payment_order_id.js
@@ -0,0 +1,9 @@
+app.order_get_payment_order_id = {
+    get_id: 'app_order_get_payment_order_id'
+};
+
+yangaiche(app.order_get_payment_order_id.get_id, function() {
+    return function(id, callback) {
+        callback(id);
+    };
+});
