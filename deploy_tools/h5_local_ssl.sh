21adb4a9913ca62a87b89d60975efb1af2680bd6
CaoLiSheng <caols@y-car.com.cn>
2015-10-19 16:11:46 +0800

    ssl 版发布脚本，发布到不同的文件夹。

---
+++ deploy_tools/h5_local_ssl.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+local_dir=".."
+config="local"
+make_dir="h5-"${config}"-ssl"
+
+PATH=$PATH:./libs/
+
+bash _make_h5.sh ${local_dir} ${config} ${make_dir}
---
+++ deploy_tools/h5_staging_ssl.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+local_dir=".."
+config="staging"
+ssh_config="staging"
+make_dir="h5-"${config}"-ssl"
+
+PATH=$PATH:./libs/
+
+bash _make_h5.sh ${local_dir} ${config} ${make_dir} &&
+
+bash _ui.sh ${local_dir}/${make_dir} ${ssh_config} "/data/apps/" &&
+
+bash _git.sh ${local_dir} ${config}
