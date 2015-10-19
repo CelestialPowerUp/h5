53a84d1f85a296d2cfe476d02bd369bee75a05c3
CaoLiSheng <caols@y-car.com.cn>
2015-10-19 17:43:26 +0800

    ssl deploy 6

--- deploy_tools/h5_develop_ssl.sh
+++ deploy_tools/h5_develop_ssl.sh
@@ -7,8 +7,8 @@ make_dir="h5-"${config}"-ssl"
 
 PATH=$PATH:./libs/
 
-bash _make_h5.sh ${local_dir} ${config} ${make_dir} &&
+bash _make_h5.sh ${local_dir} ${config} ${make_dir} "-ssl"  &&
 
-bash _ui.sh ${local_dir}/${make_dir} ${ssh_config} "/data/apps/" "-ssl" &&
+bash _ui.sh ${local_dir}/${make_dir} ${ssh_config} "/data/apps/"&&
 
 bash _git.sh ${local_dir} ${config}
