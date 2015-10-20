c1830195ac35a848e1527ff31a43147373a5d8ab
CaoLiSheng <caols@y-car.com.cn>
2015-10-20 12:04:30 +0800

    百度地图不支持https.

---
+++ webapp/js/lib/baidumap.js
@@ -0,0 +1,1 @@
+(function(){ window.BMap_loadScriptTime = (new Date).getTime(); document.write('<script type="text/javascript" src="https://api.map.baidu.com/getscript?v=2.0&ak=WVAXZ05oyNRXS5egLImmentg&services=&t=20151014112051"></script>');})();
