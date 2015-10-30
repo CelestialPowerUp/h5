a04e3d9d94b61e87c97f0032c9324b83b34dc26e
CaoLiSheng <caols@y-car.com.cn>
2015-10-30 11:32:48 +0800

    时间选择

---
+++ webapp/js/default/activity_order_create_init.js
@@ -0,0 +1,11 @@
+yangaiche(sys.init)(function(t) {
+    var swiper = new Swiper('.swiper-container', {
+        direction: 'horizontal',
+        scrollbar: '.swiper-scrollbar',
+        slidesPerView: 'auto',
+        scrollbarHide: true,
+        centeredSlides: false,
+        grabCursor: true,
+        freeMode: true
+    });
+});
