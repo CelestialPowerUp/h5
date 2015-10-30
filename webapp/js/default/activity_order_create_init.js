76ed08b1ca8fe6c8e95892bc5fe691b3737c0d23
CaoLiSheng <caols@y-car.com.cn>
2015-10-30 15:11:24 +0800

    支付按钮事件

--- webapp/js/default/activity_order_create_init.js
+++ webapp/js/default/activity_order_create_init.js
@@ -11,13 +11,37 @@ yangaiche(sys.init)(function (t) {
         t('#picker').show();
 
         var swiper = swiper || new Swiper('.swiper-container', {
-            direction: 'horizontal',
-            scrollbar: '.swiper-scrollbar',
-            slidesPerView: 'auto',
-            scrollbarHide: true,
-            centeredSlides: false,
-            grabCursor: true,
-            freeMode: true
+                direction: 'horizontal',
+                scrollbar: '.swiper-scrollbar',
+                slidesPerView: 'auto',
+                scrollbarHide: true,
+                centeredSlides: false,
+                grabCursor: true,
+                freeMode: true
+            });
+    });
+
+    var pay_btn_group = [t('#alipay_btn'), t('#wechat_btn')];
+    t.each(pay_btn_group, function (i, btn) {
+        function other_btn() {
+            var ret = [];
+            var len = pay_btn_group.length;
+            for (var j = 0; j < len; j++) {
+                if (i !== j) {
+                    ret.push(pay_btn_group[j]);
+                }
+            }
+            return ret;
+        }
+
+        var weixuan = 'url("../pubimg/new/weixuan.png") 50% 50% no-repeat';
+        var gouxuan = 'url("../pubimg/new/gouxuan.png") 50% 50% no-repeat';
+
+        btn.click(function () {
+            t.each(other_btn(), function (k, other) {
+                other.find('.choose').css('background', weixuan);
+            });
+            btn.find('.choose').css('background', gouxuan);
         });
     });
 });
