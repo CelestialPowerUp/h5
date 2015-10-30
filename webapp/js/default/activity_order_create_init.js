5850eac51918d8875d50a166e88569bd326de9e6
CaoLiSheng <caols@y-car.com.cn>
2015-10-30 14:41:34 +0800

    添加调取时间的事件

--- webapp/js/default/activity_order_create_init.js
+++ webapp/js/default/activity_order_create_init.js
@@ -1,11 +1,23 @@
-yangaiche(sys.init)(function(t) {
-    var swiper = new Swiper('.swiper-container', {
-        direction: 'horizontal',
-        scrollbar: '.swiper-scrollbar',
-        slidesPerView: 'auto',
-        scrollbarHide: true,
-        centeredSlides: false,
-        grabCursor: true,
-        freeMode: true
+yangaiche(sys.init)(function (t) {
+    t('#dimmer').click(function () {
+        t('#dimmer').hide();
+        t('#legend').hide();
+        t('#picker').hide();
+    });
+
+    t('#pick_time').click(function () {
+        t('#dimmer').show();
+        t('#legend').show();
+        t('#picker').show();
+
+        var swiper = swiper || new Swiper('.swiper-container', {
+            direction: 'horizontal',
+            scrollbar: '.swiper-scrollbar',
+            slidesPerView: 'auto',
+            scrollbarHide: true,
+            centeredSlides: false,
+            grabCursor: true,
+            freeMode: true
+        });
     });
 });
