398bf7c01fd86bafbc9a284dfae20b926853d0ed
曹力升 <caols@y-car.com.cn>
2016-02-29 18:39:48 +0800

    商品详情页，界面。

--- webapp/js/default/init/store_item_page_init.js
+++ webapp/js/default/init/store_item_page_init.js
@@ -32,9 +32,6 @@
 
         var device_width = t(window).width();
 
-        //var raw_data = '{"data":{"ware_type_id":1,"ware_status":"down_shelves","ware_id":1,"introduction_imgs":[{"img_id":1052,"img_index":0,"original_url":"http://7xiqd7.com2.z0.glb.qiniucdn.com/1052.jpg/s1024.jpg","raw_url":"http://7xiqd7.com2.z0.glb.qiniucdn.com/1052.jpg","thumbnail_url":"http://7xiqd7.com2.z0.glb.qiniucdn.com/1052.jpg/s250.jpg"}],"ware_full_price":3232,"ware_type_name":"室内清洗","cover_img_id":1052,"product_id":2,"product_info":"机油-磁护","ware_mark_price":94.5,"ware_name":"室内清洗","brief_introduction":"简介备注","cover_img":{"thumbnail_url":"http://7xiqd7.com2.z0.glb.qiniucdn.com/1052.jpg/s250.jpg","img_index":0,"img_id":1052,"original_url":"http://7xiqd7.com2.z0.glb.qiniucdn.com/1052.jpg/s1024.jpg","raw_url":"http://7xiqd7.com2.z0.glb.qiniucdn.com/1052.jpg"},"ware_products":[{"product_price":94.5,"product_id":2,"product_name":"磁护","product_category":"机油"}]},"code":"00000"}';
-        //var data = JSON.parse(raw_data).data;
-
         function make_array(size) {
             var array = [];
             for (var i = 0; i < size; i++) {
@@ -45,43 +42,42 @@
 
         var getReq = yangaiche(app.http.get_request),
             show_msg = yangaiche(app.show_msg.show),
-            product_ids = '';
+            storage = yangaiche(sys.local_storage),
+            gsth;
 
-        function load_comments(store_item) {
-            t.each(store_item.ware_products, function (i, wp) {
-                if (i !== 0) {
-                    product_ids += ',';
-                }
-                product_ids += wp.product_id;
-            });
-
-            yangaiche(app.paging.setup)({
-                url_request: '/v2/api/order/service_comment/page_list.json?product_ids=' + product_ids,
-                data_handler: function (data) {
-                    t.each(data.items, function (i, d) {
-                        d.order_rating = make_array(d.service_rating);
-                        d.keeper_rating = make_array(d.keeper_rating);
-                        d.create_time = d.create_time.substr(0, (4 + 2 + 1 + 2 + 1));
-                        d.comment_user_name = d.comment_user_name.substr(0, 1) + '**';
-                        d.comment_text = yangaiche(app.format.stripscript)(d.comment_text);
-                    });
-
-                    var tpl = Handlebars.compile(t('#store_item_comment_tpl').text());
-                    t('#store-item-comments ul').append(tpl(data.items));
-
-                    var total_height = 0;
-                    t.each(t('#store-item-comments ul li'), function (i, l) {
-                        var list = t(l);
-                        var height = (list.children('p').eq(0).height() + 70 * 2);
-                        total_height += height;
-                        list.css('height', height + 'px');
-                    });
-                    t('#store-item-comments ul').height(total_height);
-                }
-            });
-        }
-
-        var gsth;
+        //function load_comments(store_item) {
+        //    t.each(store_item.ware_products, function (i, wp) {
+        //        if (i !== 0) {
+        //            product_ids += ',';
+        //        }
+        //        product_ids += wp.product_id;
+        //    });
+        //
+        //    yangaiche(app.paging.setup)({
+        //        url_request: '/v2/api/order/service_comment/page_list.json?product_ids=' + product_ids,
+        //        data_handler: function (data) {
+        //            t.each(data.items, function (i, d) {
+        //                d.order_rating = make_array(d.service_rating);
+        //                d.keeper_rating = make_array(d.keeper_rating);
+        //                d.create_time = d.create_time.substr(0, (4 + 2 + 1 + 2 + 1));
+        //                d.comment_user_name = d.comment_user_name.substr(0, 1) + '**';
+        //                d.comment_text = yangaiche(app.format.stripscript)(d.comment_text);
+        //            });
+        //
+        //            var tpl = Handlebars.compile(t('#store_item_comment_tpl').text());
+        //            t('#store-item-comments ul').append(tpl(data.items));
+        //
+        //            var total_height = 0;
+        //            t.each(t('#store-item-comments ul li'), function (i, l) {
+        //                var list = t(l);
+        //                var height = (list.children('p').eq(0).height() + 70 * 2);
+        //                total_height += height;
+        //                list.css('height', height + 'px');
+        //            });
+        //            t('#store-item-comments ul').height(total_height);
+        //        }
+        //    });
+        //}
 
         function get_unique_service_type(sth) {
             if (undefined !== sth) {
@@ -92,12 +88,6 @@
         }
 
         function init(suppliers, service_products) {
-            if (suppliers.length > 0) {
-                t('#store-item-supplier span:last-child').text(suppliers[0].supplier_name);
-            } else {
-                t('#store-item-supplier span:last-child').text('养爱车综合店');
-                t('#my-btn-group button[data-key="self"]').css('display', 'none');
-            }
 
             t.each(service_products, function (i, service_product) {
                 service_product.total_price = yangaiche(ls.products.calculate_single)(service_product).toFixed(2);
@@ -116,8 +106,8 @@
                 storage.set(get_unique_service_type(), $this.attr('data-key'));
                 t('#store-item-service-type span').text($this.text());
 
-                const products = yangaiche(ls.products.touch)();
-                const mutable_products = yangaiche(app.obj_util.copy)(products);
+                var products = yangaiche(ls.products.touch)();
+                var mutable_products = yangaiche(app.obj_util.copy)(products);
                 t.each(service_products, function (i, service_product) {
                     if (service_product.service_type === local_key) {
                         mutable_products.push(service_product);
@@ -147,13 +137,19 @@
                 t('#item_image_cover').attr('src', store_item.cover_img.raw_url + '?imageView2/3/w/180/h/180/interlace/1');
 
                 store_item.cover_img.raw_url = store_item.cover_img.raw_url + '?imageView2/3/w/' + parseInt(device_width) + '/h/' + parseInt(device_width / 16 * 9) + '/interlace/1';
+                store_item.supplier_name = suppliers.length > 0 ? suppliers[0].supplier_name : '养爱车综合店';
 
                 var tpl = Handlebars.compile(t('#store_item_page_tpl').text());
                 t('body').prepend(tpl(store_item));
 
-                t('#item_title').text(store_item.ware_name);
+                t.each(store_item.introduction_imgs, function (i, img) {
+                    img.raw_url = img.raw_url + '?imageView2/3/w/' + parseInt(device_width) + '/h/' + parseInt(device_width / 16 * 9) + '/interlace/1';
+                });
 
-                load_comments(store_item);
+                tpl = Handlebars.compile(t('#store_item_detail_tpl').text());
+                t('#store-item-details').html(tpl(store_item));
+
+                t('#item_title').text(store_item.ware_name);
 
                 t('#submit_btn').click(function () {
                     if (!yangaiche(sys.exist)(store_item)) {
@@ -231,20 +227,13 @@
             t('#popup').css('display', 'block');
         }
 
-        t('#store-item-service-type').click(ready_submit);
-
-        t('#ready_submit').click(ready_submit);
-
         t('.store-item-cover').click(function () {
             t('#popup').css('display', 'none');
         });
 
-        var storage = yangaiche(sys.local_storage);
-
-
         var car_info = storage.get(key.car.info);
         if (yangaiche(sys.exist)(car_info)) {
-            t('#store-item-car-choose span').text(car_info.model);
+            t('#store-item-car-choose .car-info-text').text(car_info.model);
             var short_model = car_info.model.length > 10 ? car_info.model.substr(0, 10) + '...' : car_info.model;
             t('#car_model').text(short_model);
         }
