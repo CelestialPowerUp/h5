1081a7ec6c6ec7b1279eb85caea6736f8b764949
曹力升 <caols@y-car.com.cn>
2016-03-04 10:56:20 +0800

    协议内容

--- webapp/js/default/show_msg.js
+++ webapp/js/default/show_msg.js
@@ -2,7 +2,7 @@
 
     'use strict';
 
-	yangaiche(sys.load_default_module)('repository');
+    yangaiche(sys.load_default_module)('repository');
 
     app.show_msg = {
         init: 'init_show_msg',
@@ -20,7 +20,18 @@
         '<div tabindex="-1" id="yac-modal">' +
         '<div class="modal-body white fixed">' +
         '<div class="modal-title">养爱车声明协议</div>' +
-        '<div class="modal-text">{{car_number}}</div>' +
+        '<div class="modal-text">' +
+        '<p>为保证服务安全、有序、顺利地实施，甲乙双方本着相互支持、相互理解的原则，就接送车服务达成如下协议：</p>' +
+        '<p>1.甲方在甲乙双方协商约定的时间内向乙方提供车号为：（{{car_number}}）的汽车代驾接送车服务。</p>' +
+        '<p>2.服务前，乙方必须出示真实有效的行驶本和商业险（车损险）及交强险保单正本。如车辆为外地牌照还需要提供真实有效的进京证等相关手续。如无法提供任何一项相关证件，服务管家有权拒绝提供代驾服务。</p>' +
+        '<p>3.甲方应按指定路段进行，过程中车速不得超过限速规定。</p>' +
+        '<p>4.甲方提供服务时，必须遵守国家规定的道路交通法规相关之规定。</p>' +
+        '<p>5.如因管家司机不遵守交通法规，发生交通违章，由甲方及时到交通管理部门，接受处理。</p>' +
+        '<p>6.如因甲方管家司机不遵守交通法规（代驾协议）而造成交通事故，由甲方承担事故责任。如将代驾车辆损坏，应首先以代驾车辆的商业及交强险进行保险理赔，甲方承担本次因保险事故造成的保费上浮及替代用车等赔偿费用。</p>' +
+        '<p>7.甲方保留随时因车辆安全问题终止代驾服务的权利。</p>' +
+        '<p>8.雨雪或大风等恶劣天气 甲方有权拒绝乙方的代驾请求。</p>' +
+        '<p>9.服务路线由甲方划定，乙方有知情权（车辆定位）。</p>' +
+        '</div>' +
         '<div class="modal-btns two">' +
         '<div class="modal-btn cancel">取消</div>' +
         '<div class="modal-btn confirm">同意</div>' +
