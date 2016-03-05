682ee21d0b19e6ee04baacfaf60de50573e27ec6
曹力升 <caols@y-car.com.cn>
2016-03-05 23:32:22 +0800

    社区半径首页

--- webapp/js/shequbanjing/init/store_init.js
+++ webapp/js/shequbanjing/init/store_init.js
@@ -2,10 +2,11 @@
 
     'use strict';
 
-	yangaiche(sys.load_default_module)('store_init');
+	//yangaiche(sys.load_default_module)('init/store_init');
     yangaiche(sys.load_default_module)('http');
     yangaiche(sys.load_default_module)('parameter');
     yangaiche(sys.load_default_module)('user');
+    yangaiche(sys.load_default_module)('back');
     yangaiche(sys.load_default_module)('show_msg');
 
     yangaiche(sys.init)(function (t) {
@@ -15,15 +16,16 @@
 
         yangaiche(app.http.post_request)('/v1/api/radius/auto_login.json', yangaiche(app.url_parameter), function (data) {
             yangaiche(ls.user.set)(data);
+            yangaiche(ls.back.set_back_to_self)('shequbanjing_store.html');
         }, function (error) {
             yangaiche(app.show_msg.show)(error.message || JSON.stringify(error));
         });
 
         yangaiche(app.http.tweak)(function (type, request_type, url) {
             if (type === app.http.abort_or_hijack) {
-                if (url === '/v2/api/store/home_ware_list.json') {
-                    return '/v1/api/radius/get_goods_info.json';
-                }
+                //if (url === '/v2/api/store/home_ware_list.json') {
+                //    return '/v1/api/radius/get_goods_info.json';
+                //}
                 //return url;
                 return app.http.abort;
             }
