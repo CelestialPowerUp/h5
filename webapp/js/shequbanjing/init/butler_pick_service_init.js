834451025d7dcf068169737295968103d344bdac
曹力升 <caols@y-car.com.cn>
2016-03-07 13:53:03 +0800

    减少不必要的登录请求。

--- webapp/js/shequbanjing/init/butler_pick_service_init.js
+++ webapp/js/shequbanjing/init/butler_pick_service_init.js
@@ -10,10 +10,29 @@
     //yangaiche(sys.load_lib_module)('');
 
     yangaiche(sys.init)(function (t) {
+
+        var get_user = yangaiche(ls.user.touch);
+
+        yangaiche(app.http.tweak)(function (type, request_type, url) {
+            if (type === app.http.abort_or_hijack) {
+                if (url === '/v1/api/radius/auto_login.json') {
+                    var user = get_user();
+                    if (user) {
+                        return app.http.abort;
+                    }
+                }
+                return url;
+            }
+        });
+
         yangaiche(app.http.post_request)('/v1/api/radius/auto_login.json', yangaiche(app.url_parameter), function (data) {
             yangaiche(ls.user.set)(data);
         }, function (error) {
             yangaiche(app.show_msg.show)(error.message || JSON.stringify(error));
+        });
+
+        t('#store-item-car-choose').click(function () {
+            return Boolean(get_user());
         });
     }, 0);
 }());
