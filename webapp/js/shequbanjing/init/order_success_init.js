4654cbf152a7f8b834aed49a13bee6f6fa296a01
曹力升 <caols@y-car.com.cn>
2016-03-07 10:19:59 +0800

    图片显示设置为铺满div

--- webapp/js/shequbanjing/init/order_success_init.js
+++ webapp/js/shequbanjing/init/order_success_init.js
@@ -2,7 +2,7 @@
 
     'use strict';
 
-	yangaiche(sys.load_default_module)('order_success_init');
+	yangaiche(sys.load_default_module)('init/order_success_init');
     yangaiche(sys.load_default_module)('back');
 
     yangaiche(sys.init)(function (t) {
--- webapp/js/shequbanjing/init/store_init.js
+++ webapp/js/shequbanjing/init/store_init.js
@@ -11,24 +11,31 @@
 
     yangaiche(sys.init)(function (t) {
 
+        var get_user = yangaiche(ls.user.touch),
+            set_back_to_self = yangaiche(ls.back.set_back_to_self);
+
         t('#to_show_user_win').remove();
         t('#home-page-wrapper').empty().css('display', 'block');
 
-        yangaiche(app.http.post_request)('/v1/api/radius/auto_login.json', yangaiche(app.url_parameter), function (data) {
-            yangaiche(ls.user.set)(data);
-            yangaiche(ls.back.set_back_to_self)('shequbanjing_store.html');
-        }, function (error) {
-            yangaiche(app.show_msg.show)(error.message || JSON.stringify(error));
-        });
-
         yangaiche(app.http.tweak)(function (type, request_type, url) {
             if (type === app.http.abort_or_hijack) {
-                //if (url === '/v2/api/store/home_ware_list.json') {
-                //    return '/v1/api/radius/get_goods_info.json';
-                //}
-                //return url;
+                if (url === '/v1/api/radius/auto_login.json') {
+                    var user = get_user();
+                    if (user) {
+                        set_back_to_self('shequbanjing_store.html');
+                        return app.http.abort;
+                    }
+                    return url;
+                }
                 return app.http.abort;
             }
+        });
+
+        yangaiche(app.http.post_request)('/v1/api/radius/auto_login.json', yangaiche(app.url_parameter), function (data) {
+            yangaiche(ls.user.set)(data);
+            set_back_to_self('shequbanjing_store.html');
+        }, function (error) {
+            yangaiche(app.show_msg.show)(error.message || JSON.stringify(error));
         });
 
     }, 0);
