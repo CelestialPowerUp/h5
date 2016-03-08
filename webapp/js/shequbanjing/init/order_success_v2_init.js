5f7618fc4a00411401f488f4c9c07c2a7e5bd1b8
曹力升 <caols@y-car.com.cn>
2016-03-08 16:25:55 +0800

    order_success -> order_success_v2

--- webapp/js/shequbanjing/init/order_success_v2_init.js
+++ webapp/js/shequbanjing/init/order_success_v2_init.js
@@ -2,7 +2,7 @@
 
     'use strict';
 
-	yangaiche(sys.load_default_module)('init/order_success_init');
+	yangaiche(sys.load_default_module)('init/order_success_v2_init');
     yangaiche(sys.load_default_module)('back');
 
     yangaiche(sys.init)(function (t) {
