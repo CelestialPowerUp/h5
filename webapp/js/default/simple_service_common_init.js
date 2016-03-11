2c4a2bdb73cfdcf8a85752f9ee8af3a71b7be014
曹力升 <caols@y-car.com.cn>
2016-03-11 15:02:53 +0800

    修改商城，并拯救三项普通服务中的失足代码。

--- webapp/js/default/simple_service_common_init.js
+++ webapp/js/default/simple_service_common_init.js
@@ -8,6 +8,7 @@
     yangaiche(sys.load_default_module)('products');
     yangaiche(sys.load_default_module)('back');
     yangaiche(sys.load_default_module)('supplier');
+    yangaiche(sys.load_default_module)('unique_service_type');
 
     app.simple_service_products = {
         config: {
@@ -25,9 +26,11 @@
     yangaiche(sys.init)(function (t) {
 
         var storage = yangaiche(sys.local_storage);
-        app.simple_service_products.key = window.location.href.match(/\/.*\/(.*?)\.html/)[1];
+        var get_unique_service_type = yangaiche(app.unique_service_type.get);
 
+        app.simple_service_products.key = window.location.href.match(/\/.*\/(.*?)\.html/)[1];
         function init(service_products) {
+
             storage.set(key.service.data, service_products);
 
             storage.set(key.service.can_self, false);
@@ -36,10 +39,13 @@
                 service_type = app.simple_service_products.config[app.simple_service_products.key];
 
             //var config = yangaiche(sys.exist)(order.supplier_id) ? '&supplier_id=' + order.supplier_id : '';
-
             yangaiche(app.http.get_request)('/v2/api/products.json?service_type=' + service_type + '&car_model_type=' + order.car_model_type, function (data) {
                 if (service_products.length > 0) {
                     data.required_products.push(service_products[0]);
+
+                    storage.set(get_unique_service_type(app.simple_service_products.key), service_products[0].service_type);
+                } else {
+                    storage.set(get_unique_service_type(app.simple_service_products.key), '');
                 }
                 yangaiche(ls.products.set)(data.required_products);
 
@@ -49,21 +55,24 @@
 
             // 去支付按钮
             t('#store-item-footer .submit').click(function () {
-                var storage = yangaiche(sys.local_storage);
                 var car = storage.get(key.car.info);
                 if (!yangaiche(sys.exist)(car)) {
                     yangaiche(app.show_msg.show)('请先选车');
                     return false;
                 }
 
-                yangaiche(sys.local_storage).set(key.submit_button.submit_text_key, key.submit_button.submit_text_value3);
-                yangaiche(sys.local_storage).set(key.shequbanjin.is_butler_pick, false);
+                storage.set(key.submit_button.submit_text_key, key.submit_button.submit_text_value3);
+                storage.set(key.shequbanjin.is_butler_pick, false);
+
+                yangaiche(ls.order.update)(function (order) {
+                    order.supplier_id = null;
+                });
 
                 yangaiche(ls.back.set_back_to_self)('order_settle.html');
             });
 
             t('#store-item-car-choose').click(function () {
-                yangaiche(sys.local_storage).remove(key.goto.car_list);
+                storage.remove(key.goto.car_list);
                 yangaiche(ls.back.set_back_to_self)('car_list.html');
             });
         }
