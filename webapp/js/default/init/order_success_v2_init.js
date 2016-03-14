4b4fed075bd99dfb3d9bf5cc3fed2b762b228ccf
曹力升 <caols@y-car.com.cn>
2016-03-14 14:42:19 +0800

    支付后返回设置为首页。

--- webapp/js/default/init/order_success_v2_init.js
+++ webapp/js/default/init/order_success_v2_init.js
@@ -2,12 +2,18 @@
 
     'use strict';
 
+	yangaiche(sys.load_default_module)('back');
 	yangaiche(sys.load_default_module)('order');
     yangaiche(sys.load_default_module)('parameter');
     yangaiche(sys.load_default_module)('pay');
     yangaiche(sys.load_module)('order/get_payment_order_id');
 
     yangaiche(sys.init)(function (t) {
+        if (yangaiche(app.url_parameter).msg_key) {
+            yangaiche(ls.back.set_back_to_store)('order_success_v2.html');
+            return;
+        }
+
         t('.normal-bottom-btn-div').css('display', 'block');
         t('#pay_button').css('display', 'none');
 
