20cb90cf2c46a94ab5fe5cd50f892ba938785080
曹力升 <caols@y-car.com.cn>
2016-03-16 12:14:28 +0800

    支付界面返按下回键重复下单数据不一致问题。

--- webapp/js/default/init/order_success_v2_init.js
+++ webapp/js/default/init/order_success_v2_init.js
@@ -6,11 +6,16 @@
 	yangaiche(sys.load_default_module)('order');
     yangaiche(sys.load_default_module)('parameter');
     yangaiche(sys.load_default_module)('pay');
+    yangaiche(sys.load_default_module)('http');
     yangaiche(sys.load_module)('order/get_payment_order_id');
 
     yangaiche(sys.init)(function (t) {
-        if (yangaiche(app.url_parameter).msg_key) {
-            yangaiche(ls.back.set_back_to_store)('order_success_v2.html');
+        var order_id = yangaiche(app.url_parameter).order_id;
+        if (order_id) {
+            yangaiche(app.http.get_request)('/v3/api/orders.json?user_type=caruser&order_id=' + order_id, function (data) {
+                yangaiche(ls.order.set)(data);
+                yangaiche(ls.back.set_back_to_store)('order_success_v2.html');
+            });
             return;
         }
 
